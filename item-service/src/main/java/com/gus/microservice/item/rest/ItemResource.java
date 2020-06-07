88d26f2f503e2ecaacda1825e6c9b9c1c283a55d
zesow <anstkd07@gmail.com>
2020-06-07 20:03:54 +0900

    application.yml to remote repository

--- item-service/src/main/java/com/gus/microservice/item/rest/ItemResource.java
+++ item-service/src/main/java/com/gus/microservice/item/rest/ItemResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/item")
+@RequestMapping
 public class ItemResource {
 
     public final ItemService itemService;
